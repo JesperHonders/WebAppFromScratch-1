e2299ba273c027e35479ed6fb4cbec01a870f86d
beemstb002 <Bart@Barts-MacBook-Pro.local>
2016-02-03 18:06:40 +0100

    Assignment 5
    Uploading assignment 5.

--- opdracht5/static/app.js
+++ opdracht5/static/app.js
@@ -4,26 +4,38 @@
 	var app = {
 		init:function() {
 			routes.init();
+            console.log("initialized")
 		}
 	};
 
 	var routes = {
 		init:function() {
 			window.addEventListener("hashchange", function(HashChangeEvent){
-				console.log(HashChangeEvent);
+                
+				var hashLocationNew = HashChangeEvent.newURL.search("#");
+                var hashLocationOld = HashChangeEvent.oldURL.search("#");
+                var newHash = HashChangeEvent.newURL.substr(hashLocationNew);
+                var oldHash = HashChangeEvent.oldURL.substr(hashLocationOld);
+                
+                sections.toggle(newHash, oldHash);
 			}, false);			
 		}
 	};
 
 	var sections = {
-		toggle: function (route) {
-			if(route === 'lijst') {
-
-			} else {
+		toggle: function (newHash, oldHash) {
+			if(newHash === '#page2') {
+                document.getElementById("list").style.display = "block";
+                document.getElementById("home").style.display = "none";
+			}
+            else if(newHash === '#page1'){
+                document.getElementById("home").style.display = "block";
+                document.getElementById("list").style.display = "none";
+            }
+            else {
 
 			}
 		}
 	}
-
 	app.init();
 }());
